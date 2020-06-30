f368b3bd2b3728f55fc7cb1f6b510e66487c49c9
Juho Alin <juhoalin@icloud.com>
2020-06-30 18:31:59 +0300

    Initial Commit

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Juho Alin on 30.6.2020.
+//  Copyright © 2020 Juho Alin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
